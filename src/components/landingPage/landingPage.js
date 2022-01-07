19e8232de0654793186811869f3732199c7da37a
Ravisha Sharma <ravisha7feb@gmail.com>
2022-01-08 00:32:05 +0530

    add more elements of landing page

--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -6,6 +6,17 @@ class LandingPage extends React.Component {
     render (){
         return (
             <div className="landing-page">
+                <div className="landing-page-left">
+                    <div className="landing-page-left-header">
+                        What do you MEME?
+                    </div>
+                    <div className="landing-page-left-description">
+                        Ready to "meme" along your buddies?
+                    </div>
+                    <div className="landing-page-left-button">
+                        <button>GET STARTED</button> 
+                    </div>
+                </div>
                 <div className="landing-page-bg">
                     <img src={bgImg} alt="bg"></img>
                 </div>
