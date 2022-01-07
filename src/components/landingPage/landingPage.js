110ac62d2a24e699009a64acf9bf77ba34fc4e74
Ravisha Sharma <ravisha7feb@gmail.com>
2022-01-07 22:09:58 +0530

    add image fix styles

--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,11 +1,15 @@
 import React from "react"
 import "./landingPage.css"
+import bgImg from "../../images/landingImg.png"
 
 class LandingPage extends React.Component {
     render (){
         return (
-            <div>
-                <p> Landing Page coming soon</p>
+            <div className="landing-page">
+                <div className="landing-page-bg">
+                    <img src={bgImg} alt="bg"></img>
+                </div>
+                
             </div>
         );
     }
