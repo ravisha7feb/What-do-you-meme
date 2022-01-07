474fe8e0cd6f855b174492d6890db4002305c60f
Ravisha Sharma <ravisha7feb@gmail.com>
2022-01-07 21:49:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LandingPage from './components/landingPage/landingPage'
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/landingPage/landingPage.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./landingPage.css"
+
+class LandingPage extends React.Component {
+    render (){
+        return (
+            <div>
+                <p> Landing Page coming soon</p>
+            </div>
+        );
+    }
+
+}
+
+export default LandingPage;
