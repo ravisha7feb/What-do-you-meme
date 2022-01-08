1452de6e354c96366306c8b2c993e478bc19ab5c
sonalsk <sonalkushwaha039@gmail.com>
2022-01-08 17:23:33 +0530

    Add Memes DB

--- models/Scenario.js
+++ models/Scenario.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose     = require('mongoose'),
+      random       = require('mongoose-simple-random');
 
 const ScenarioSchema = new mongoose.Schema({
     scene: String
 });
+ScenarioSchema.plugin(random);
 
 module.exports = mongoose.model('Scenario', ScenarioSchema);
