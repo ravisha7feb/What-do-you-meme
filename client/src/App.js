9b341da741b764ab1d790b86bb819ac42f89d26f
Ravisha Sharma <ravisha7feb@gmail.com>
2022-01-08 17:20:45 +0530

    react-router installed and added imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
+import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import LandingPage from './components/landingPage/landingPage'
+import SecPage from './components/createJoinRoom/createJoinRoom';
 
 function App() {
   return (
     <div className="App">
+      {/*<Routes>*/}
       <LandingPage/>
+      <SecPage/>
+      {/* </Routes> */}
+      
     </div>
   );
 }
