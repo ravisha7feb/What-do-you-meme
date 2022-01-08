00f97e9a614b698e16841114eb3935efa0dd077e
sonalsk <sonalkushwaha039@gmail.com>
2022-01-08 14:50:49 +0530

    Setup mongoose and express

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express    = require('express');
 const mongoose   = require('mongoose');
 const bodyParser = require('body-parser');
+const keys       = require('./config/keys');
+const scenarios  = require('./routes/api/scenarios');
 const app        = express();
 
-app.get('/', (req, res) => {
-    res.send('hello')
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose
+    .connect(keys.mongoURI)
+    .then(() => console.log('Database connected'))
+    .catch(() => console.log(err));
+
+app.use('/api/scenarios', scenarios);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
